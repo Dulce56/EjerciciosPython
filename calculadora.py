1fc5d572d9c7d4aeb82effce21bdacce1eef444b
DulceMartinezG <55851566+Dulce56@users.noreply.github.com>
2020-02-24 11:13:32 -0600

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,38 @@
+# Creando clase calculadora
+import math
+class Calculadora:
+    def __init__(self):
+        self.a=0
+        self.b=0
+        self.r=0
+    def suma(self):
+        self.r= self.a+self.b
+    def resta(self):
+        self.r= self.a-self.b
+    def multiplicacion(self):
+        self.r= self.a*self.b
+    def divicion(self):
+        self.r= self.a/self.b
+    def potencia(self):
+        self.r= math.pow(self.a,self.b)
+    def raiz(self):
+        self.r= math.sqrt(self.a)    
+        
+        
+c=Calculadora()
+c.a=10
+c.b=20
+
+
+c.suma()
+print(c.r)
+c.resta()
+print(c.r)
+c.multiplicacion()
+print(c.r)
+c.divicion()
+print(c.r)
+c.potencia()
+print(c.r)
+c.raiz()
+print(c.r)
